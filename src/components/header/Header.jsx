687593db44d3039ac12e849b7b4dfa8e1a663a53
marcochiaro <mchiaro79@gmail.com>
2022-06-11 17:47:00 -0300

    commit con foto mia

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import ME from "../../assets/me.png";
+import ME from "../../assets/IMG_9829.JPG";
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
@@ -15,7 +15,7 @@ const Header = () => {
         <HeaderSocials />
 
         <div className="me">
-          <img src={ME} alt="" />
+          <img src={ME} alt="" className="me-2" />
         </div>
 
         <a href="#contact" className="scroll__down">
